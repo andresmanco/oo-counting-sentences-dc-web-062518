b853e222d2cc73cd3eeba4142ea98839124d8250
Andres Manco <andresmanco@gmail.com>
2018-06-28 13:59:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,12 +19,29 @@ class String
     backup = []
     self.split('.').each do |arr|
       arr.split('!').each do |arr2|
+<<<<<<< HEAD
         array << arr2.split('?')
         backup << array.flatten
       end
     end
     backup.count
    
+=======
+        binding.pry
+      #array << arr.split('!')
+      #arr
+      end
+    end
+    
+    #array.each do |arr2|
+     # arr2.each do |arr3|
+    #  if arr3 != ''
+      #end
+#    end
+  # end
+    #backup.flatten.count
+  
+>>>>>>> e02bcca720adfcf809ff19fdaa5aa41a5e5a662e
     
   end
 end
